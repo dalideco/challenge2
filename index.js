d8323bea4594f6ee5278ba87b5816d0922ac4af6
dali <dalissj1@gmail.com>
2021-03-13 13:23:43 +0100

    more styling

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 
+const myText = document.querySelector("#text");
+const [police,couleur,taille ]=document.querySelectorAll(".myflex >*");
+
 setInterval(() => {
-    document.getElementById("text").style.color = document.getElementById("couleur").value;
-    document.getElementById("text").style.fontSize = `${document.getElementById("taille").value}px`;
-    document.getElementById("text").style.fontFamily = document.getElementById("police").value;
-}, 10);
+    myText.style.color = couleur.value;
+    myText.style.fontSize = `${taille.value}px`;
+    myText.style.fontFamily = police.value;
+}, 1000/30);
+
+
+
+//  const framePassed =(callback)=>{
+//     setInterval(callback, 1000/30);
+//  }
+
+//  function keepTrack(){
+//     document.getElementById("text").style.color = document.getElementById("couleur").value;
+//     document.getElementById("text").style.fontSize = `${document.getElementById("taille").value}px`;
+//     document.getElementById("text").style.fontFamily = document.getElementById("police").value;
+//     framePassed(keepTrack);
+//  }
+
+//  keepTrack();
